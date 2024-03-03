fe8a2174de004bbe88856b16886113e1585bacb0
amir79 <clash.amir.a@gmail.com>
2024-03-03 13:35:06 +0330

    add project2

---
+++ vue-ticket/src/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="header">
+     <div class="icon">
+        <p><i class="bi bi-vimeo ms-2"></i>مستر بلیط</p>
+     </div>
+        <ul class="links">
+            <router-link :to="{name:'plane'}"><li class="link"><i class="bi bi-airplane ms-2"></i>هواپیما</li></router-link>
+            <router-link :to="{name:'train'}"><li class="link"><i class="bi bi-train-front ms-2"></i>قطار</li></router-link>
+            <router-link :to="{name:'bus'}"><li class="link"><i class="bi bi-bus-front ms-2"></i>اتوبوس</li></router-link>
+            <router-link :to="{name:'hotel'}"><li class="link"><i class="bi bi-buildings ms-2"></i>هتل</li></router-link>
+        </ul>
+     
+               
+                  
+                  <ul ul class="login">
+                  <li class="log"><i class="bi bi-person-plus ms-2"></i><router-link :to="{name:'register'}">ثبت نام</router-link></li>
+                  <li class="log"><i class="bi bi-person ms-2"></i><router-link :to="{name:'login'}" >ورود</router-Link></li>
+                 </ul>
+            
+      
+     
+
+
+  </header>
+</template>
+
+<script setup>
+ 
+
+
+</script>
+
+<style scoped>
+ .header{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-bottom: 1px solid white;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+    border-radius:0% 0% 10% 10%;
+    background-color: white;
+   
+ }
+ 
+ .links{
+    display: flex;
+    margin-right: 3rem;
+ }
+  ul a li{
+   text-decoration: none;
+ }
+ .link{
+  margin-left: 3rem;
+ }
+ .links li{
+    color: #617d98;
+ }
+ .links a{
+   text-decoration: none;
+   font-weight: bold;
+   margin-right: 1.5rem;
+ }
+ .login{
+    display: flex;
+ }
+ .log{
+    margin-left: 2rem;
+ }
+ .icon .bi{
+   color: #ffc107;
+   font-size:3rem
+ }
+ .icon p{
+    font-weight: bold;
+    color: black;
+    font-size: 1.5rem;
+ }
+ .login li a{
+    text-decoration: none;
+    color:#617d98;
+    font-weight: bold;
+ }
+ .links a li:hover{
+   color:#ffc107;
+   transition: .7s;
+ }
+ .login li a:hover{
+   color: #ffc107;
+   transition: .7s;
+ }
+</style>
---
+++ vue-ticket/src/components/login.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="section">
+        <div class="row justify-content-center margin">
+            <div class="col-md-4">
+               
+                <form @submit.prevent="submit()" class=" form">
+                    <div v-if="Error.length > 0"  class="mb-5" role="alert">
+                    <ul class="mb-0">
+                        <li style="color: red;" v-for="(Errors, index) in Error" :key="index">{{ Errors }}</li>
+                    </ul>
+                   </div>
+                   
+                    <div class="mb-3">
+                        <label htmlFor="email" class="form-label">ایمیل</label>
+                        <input type="text" class="form-control" id="email" v-model="FormData.email"/>
+
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="password" class="form-label">رمز عبور</label>
+                        <input type="password" class="form-control" id="password" v-model="FormData.password"/>
+
+                    </div>
+                    
+                    <button class="btn btn-warning" type="submit">
+                        ورود
+                        <div v-if="loding" class="spinner-border spinner-border-sm ms-2"></div>
+                    </button>
+                </form>
+            </div>
+        </div>
+    
+    </div>
+</template>
+<script setup>
+
+import Swal from 'sweetalert2'
+import axios from "axios";
+const loding=ref(false);
+import { reactive , ref } from "vue"
+import { useRouter } from 'vue-router'
+const Error=ref([]);
+
+const router=useRouter();
+
+
+  const FormData=reactive({
+   
+   
+   
+    email:'',
+   
+   
+    password:'',
+   
+   
+   
+  
+  })
+  
+   function submit(){
+    loding.value=true
+    axios
+        .post("http://localhost:8000/api/login", {
+           
+            email:FormData.email,
+            password:FormData.password,
+          
+           headers: {
+              'Accept': 'application/json',
+           },
+        })
+        .then(function (response) {
+          // handle success
+          login();
+          router.push('/');
+          
+        })
+        .catch(function (error) {
+          // handle error
+              Error.value=Object.values(error.response.data).flat();
+              
+    
+            loding.value=false
+
+
+        });
+      
+
+
+   // axios({
+ // method: 'post',
+  //url: 'http://localhost:8000/api/register',
+  //data: {
+  //  name:FormData.name,
+  //  email:FormData.email,
+    //password:FormData.password,
+    //c_password:FormData.c_password
+  //},
+ // headers: {
+           //     'Accept': 'application/json',
+       //     },
+
+//});
+
+   }
+
+   function login(){
+    Swal.fire({
+            text: "شما وارد حساب کاربری خود شدید",
+            icon: "success",
+            background:'green',
+            iconColor:'white',
+            color:'white',
+            showConfirmButton: false,
+            timerProgressBar: true,
+            timer: 3000,
+            toast: true,
+            position: 'top-right',
+        });
+  }
+  
+</script>
+<style scoped>
+
+.section{
+    background-image: url('/image/train.webp');
+    background-size: cover;
+  
+    width: 95rem;
+    background-repeat: none;
+    
+}
+.form{
+    padding-left: 5rem;
+    padding-right: 5rem;
+    padding-bottom: 2rem;
+    background-color: hsl(0, 0%, 100%,.7);
+    margin-bottom: 3rem;
+    margin-top: 3rem;
+}
+.margin{
+    margin-top: 8rem;
+}
+.btn{
+    padding: .5rem 2rem .5rem 2rem;
+    margin-right: 5.8rem;
+}
+</style>
